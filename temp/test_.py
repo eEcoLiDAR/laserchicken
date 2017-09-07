30706c38a0d2a2a71b68c7d7e3db28e9bcf6795c
cwmeijer <c.meijer@esciencecenter.nl>
2017-09-07 11:30:24 +0200

    add first test The test only tests the configuration of the test runner

---
+++ temp/test_.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test_config_test(self):
+        """ This is the first test of the project and should pass if test runner is configured correctly. """
+        pass
