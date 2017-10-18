fd681536ac5c9084cc3c787253602d3b4824b271
yifatdzigan <y.dzigan@esciencecenter.nl>
2017-10-18 13:09:47 +0200

    Update plyfile2las.py

--- laserchicken/plyfile2las.py
+++ laserchicken/plyfile2las.py
@@ -15,8 +15,8 @@ def read_ply(inFile_path):
 def ply_2_las(x_att, y_att, z_att, las_header, OutFile_path):
     '--------------------------'
     ' writes  ply file format to LAS format using laspy'
+    ' user must define am header and output file'
     '--------------------------'
-    #las_header = laspy.header.Header() #or = new_header
     outFile2 = laspy.file.File(OutFile_path, mode = "w", header = las_header)
     outFile2.X = x_att
     outFile2.Y = y_att    
