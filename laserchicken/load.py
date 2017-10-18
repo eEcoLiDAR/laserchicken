b67dc06cb6a6007c46b50dbd84778c8470f6aae9
cwmeijer <c.meijer@esciencecenter.nl>
2017-10-18 15:41:32 +0200

    add working tests for loading

--- laserchicken/load.py
+++ laserchicken/load.py
@@ -1,5 +1,7 @@
 from laserchicken import load_las
+from laserchicken import write_ply
 
 
-def load(path):
-    load_las.load(path)
+def load(input_las_path, output_ply_path):
+    pc = load_las.load(input_las_path)
+    write_ply.write(pc, output_ply_path)
--- laserchicken/test_load.py
+++ laserchicken/test_load.py
@@ -9,16 +9,27 @@ else:
     from unittest import mock
 
 
-
 class TestLoad(unittest.TestCase):
     _test_dir = 'TestLoad_dir'
     _test_file_name = '5points.las'
     _test_data_source = 'testdata'
     test_file_path = os.path.join(_test_dir, _test_file_name)
 
-    @mock.patch('laserchicken.load_las.load')
-    def test_load(self, load_las_mock):
+
+    def test_load(self):
         """ Load module should call load_las to get the file. """
         nonexistent_las = 'nonexistent.las'
-        load(nonexistent_las)
-        load_las_mock.assert_called_once_with(nonexistent_las)
+        nonexistent_ply = 'nonexistent.ply'
+        with mock.patch('laserchicken.load_las.load') as load_las_mock:
+            with mock.patch('laserchicken.write_ply.write') as write_ply_mock:
+                load(nonexistent_las, nonexistent_ply)
+                load_las_mock.assert_called_once_with(nonexistent_las)
+
+    def test_write(self):
+        """ Load module should call write_ply to get the file. """
+        nonexistent_las = 'nonexistent.las'
+        nonexistent_ply = 'nonexistent.ply'
+        with mock.patch('laserchicken.write_ply.write') as write_ply_mock:
+            with mock.patch('laserchicken.load_las.load') as load_las_mock:
+                load(nonexistent_las, nonexistent_ply)
+                write_ply_mock.assert_called_once_with(nonexistent_ply)
