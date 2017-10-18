55e6f341928ba994ac62fc7a3b8caace1abbc699
cwmeijer <c.meijer@esciencecenter.nl>
2017-10-18 15:45:32 +0200

    refactor load tests and skip one test

--- laserchicken/test_load.py
+++ laserchicken/test_load.py
@@ -15,21 +15,21 @@ class TestLoad(unittest.TestCase):
     _test_data_source = 'testdata'
     test_file_path = os.path.join(_test_dir, _test_file_name)
 
-
-    def test_load(self):
+    @mock.patch('laserchicken.write_ply.write')
+    @mock.patch('laserchicken.load_las.load')
+    def test_load(self, load_las_mock, write_ply_mock):
         """ Load module should call load_las to get the file. """
         nonexistent_las = 'nonexistent.las'
         nonexistent_ply = 'nonexistent.ply'
-        with mock.patch('laserchicken.load_las.load') as load_las_mock:
-            with mock.patch('laserchicken.write_ply.write') as write_ply_mock:
-                load(nonexistent_las, nonexistent_ply)
-                load_las_mock.assert_called_once_with(nonexistent_las)
+        load(nonexistent_las, nonexistent_ply)
+        load_las_mock.assert_called_once_with(nonexistent_las)
 
-    def test_write(self):
+    @unittest.skip('Production code not implemented yet.')
+    @mock.patch('laserchicken.write_ply.write')
+    @mock.patch('laserchicken.load_las.load')
+    def test_write(self, load_las_mock, write_ply_mock):
         """ Load module should call write_ply to get the file. """
         nonexistent_las = 'nonexistent.las'
         nonexistent_ply = 'nonexistent.ply'
-        with mock.patch('laserchicken.write_ply.write') as write_ply_mock:
-            with mock.patch('laserchicken.load_las.load') as load_las_mock:
-                load(nonexistent_las, nonexistent_ply)
-                write_ply_mock.assert_called_once_with(nonexistent_ply)
+        load(nonexistent_las, nonexistent_ply)
+        write_ply_mock.assert_called_once_with(nonexistent_ply)
