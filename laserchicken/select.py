1b1cc44f8fe663133d6bd3b316bc32a04d2d7bbc
cwmeijer <c.meijer@esciencecenter.nl>
2017-10-25 17:03:47 +0200

    select above functionality #4 + select refactor

--- laserchicken/select.py
+++ laserchicken/select.py
@@ -1,6 +1,19 @@
 import numpy as np
 
 
+def select_above(pc, attribute, threshold):
+    """
+    Return the selection of the input point cloud that contains only points with a given attribute above some value.
+    :param pc: Input point cloud
+    :param attribute: The attribute used for selection
+    :param threshold: The threshold value used for selection
+    :return: A new point cloud containing only the selected points
+    """
+    _check_valid_arguments(attribute, pc)
+    mask = pc['points'][attribute]['data'] > threshold
+    return _copy_dict(pc, mask)
+
+
 def select_below(pc, attribute, threshold):
     """
     Return the selection of the input point cloud that contains only points with a given attribute below some value.
@@ -9,19 +22,25 @@ def select_below(pc, attribute, threshold):
     :param threshold: The threshold value used for selection
     :return: A new point cloud containing only the selected points
     """
-    check_valid_arguments(attribute, pc)
+    _check_valid_arguments(attribute, pc)
     mask = pc['points'][attribute]['data'] < threshold
-    return copy_dict(pc, mask)
+    return _copy_dict(pc, mask)
 
 
-def check_valid_arguments(key, pc):
+def _check_valid_arguments(attribute, pc):
+    """
+    Raises if arguments are not valid for select_above/select_below functions.
+    :param attribute:
+    :param pc:
+    :return:
+    """
     if pc is None:
         raise ValueError('Input point cloud cannot be None.')
-    if key not in pc['points']:
-        raise ValueError('Attribute key {} for selection not found in point cloud.'.format(key))
+    if attribute not in pc['points']:
+        raise ValueError('Attribute key {} for selection not found in point cloud.'.format(attribute))
 
 
-def copy_dict(pc_in, array_mask):
+def _copy_dict(pc_in, array_mask):
     """
     Makes a deep copy of a point cloud dict using the array mask when copying the points.
     :param pc_in: Input point cloud
@@ -31,7 +50,7 @@ def copy_dict(pc_in, array_mask):
     result = {}
     for key, value in pc_in.items():
         if type(value) == dict:
-            new_value = copy_dict(value, array_mask)
+            new_value = _copy_dict(value, array_mask)
         elif type(value) == np.ndarray:
             new_value = value[array_mask]
         else:
--- laserchicken/test_select.py
+++ laserchicken/test_select.py
@@ -3,7 +3,74 @@ import unittest
 import numpy as np
 import pytest
 
-from laserchicken.select import select_below
+from laserchicken.select import select_below, select_above
+
+
+class TestSelectBelow(unittest.TestCase):
+    def test_selectBelow_None(self):
+        """ None input raises Value Error. """
+        assert_none_pc_raises_value_error(select_below)
+
+    def test_selectBelow_unknownKey(self):
+        """ If key is not in point cloud, raise Value Error. """
+        assert_unknown_key_raises_value_error(select_below)
+
+    def test_selectBelow_noneKey(self):
+        """ If key is None, raise Value Error. """
+        assert_none_key_raises_value_error(select_below)
+
+    def test_selectBelow_outputIsNotInput(self):
+        """ Even when selecting all points, the function should never return the actual input object. """
+        pc_in = get_test_data()
+        pc_out = select_below_ridiculous_high_z(pc_in)
+        assert (pc_in is not pc_out)
+
+    def test_selectBelow_everything(self):
+        """ Selecting all point under some super high value should return all the points. """
+        pc_in = get_test_data()
+        pc_out = select_below_ridiculous_high_z(pc_in)
+        self.assertEqual(len(pc_out['points']['x']['data']), 3)
+        assert_points_equal(pc_in, pc_out)
+
+    def test_selectBelow_onlyOnePoint(self):
+        """ Selecting below some threshold should only result the correct lowest point. """
+        pc_in = get_test_data()
+        pc_out = select_below(pc_in, 'z', 3.2)
+        self.assertEqual(len(pc_out['points']['x']['data']), 1)
+        np.testing.assert_almost_equal(pc_out['points']['x']['data'][0], 1.1)
+        np.testing.assert_almost_equal(pc_out['points']['y']['data'][0], 2.1)
+        np.testing.assert_almost_equal(pc_out['points']['z']['data'][0], 3.1)
+        np.testing.assert_almost_equal(pc_out['points']['return']['data'][0], 1)
+
+
+class TestSelectAbove(unittest.TestCase):
+    def test_selectAbove_None(self):
+        """ None input raises Value Error. """
+        assert_none_pc_raises_value_error(select_above)
+
+    def test_selectAbove_unknownKey(self):
+        """ If key is not in point cloud, raise Value Error. """
+        assert_unknown_key_raises_value_error(select_above)
+
+    def test_selectAbove_noneKey(self):
+        """ If key is None, raise Value Error. """
+        assert_none_key_raises_value_error(select_above)
+
+    def test_selectAbove_everything(self):
+        """ Selecting all point above some super low value should return all the points. """
+        pc_in = get_test_data()
+        pc_out = select_above(pc_in, 'z', -100000)
+        assert_points_equal(pc_in, pc_out)
+
+    def test_selectBelow_onlyOnePoint(self):
+        """ Selecting above some threshold should only result the correct highest point. """
+        pc_in = get_test_data()
+        pc_out = select_above(pc_in, 'z', 3.2)
+        self.assertEqual(len(pc_out['points']['x']['data']), 1)
+        np.testing.assert_almost_equal(pc_out['points']['x']['data'][0], 1.3)
+        np.testing.assert_almost_equal(pc_out['points']['y']['data'][0], 2.3)
+        np.testing.assert_almost_equal(pc_out['points']['z']['data'][0], 3.3)
+        np.testing.assert_almost_equal(pc_out['points']['return']['data'][0], 2)
 
 
 def get_test_data():
@@ -21,50 +88,26 @@ def select_below_ridiculous_high_z(pc_in):
 
 
 def assert_points_equal(pc_in, pc_out):
+    np.testing.assert_equal(len(pc_out['points']['x']['data']), len(pc_in['points']['x']['data']))
     np.testing.assert_almost_equal(pc_out['points']['x']['data'], pc_in['points']['x']['data'])
     np.testing.assert_almost_equal(pc_out['points']['y']['data'], pc_in['points']['y']['data'])
     np.testing.assert_almost_equal(pc_out['points']['z']['data'], pc_in['points']['z']['data'])
     np.testing.assert_almost_equal(pc_out['points']['return']['data'], pc_in['points']['return']['data'])
 
 
-class TestSelectBelow(unittest.TestCase):
-    def test_selectBelow_None(self):
-        """ None input raises Value Error. """
-        with pytest.raises(ValueError):
-            pc_in = None
-            select_below(pc_in, 'x', 13)
+def assert_none_pc_raises_value_error(function):
+    with pytest.raises(ValueError):
+        pc_in = None
+        function(pc_in, 'x', 13)
 
-    def test_selectBelow_unknownKey(self):
-        """ If key is not in point cloud, raise Value Error. """
-        pc = get_test_data()
-        with pytest.raises(ValueError):
-            select_below(pc, 'unknown_key_123', 13)
 
-    def test_selectBelow_noneKey(self):
-        """ If key is not in point cloud, raise Value Error. """
-        pc = get_test_data()
-        with pytest.raises(ValueError):
-            select_below(pc, None, 13)
+def assert_unknown_key_raises_value_error(function):
+    pc = get_test_data()
+    with pytest.raises(ValueError):
+        function(pc, 'unknown_key_123', 13)
 
-    def test_selectBelow_outputIsNotInput(self):
-        """ Even when selecting all points, the function should never return the actual input object. """
-        pc_in = get_test_data()
-        pc_out = select_below_ridiculous_high_z(pc_in)
-        assert (pc_in is not pc_out)
 
-    def test_selectBelow_everything(self):
-        """ Selecting all point under some super high value should return all the points. """
-        pc_in = get_test_data()
-        pc_out = select_below_ridiculous_high_z(pc_in)
-        self.assertEqual(len(pc_out['points']['x']['data']), 3)
-        assert_points_equal(pc_in, pc_out)
-
-    def test_selectBelow_onlyOnePoint(self):
-        """ Selecting below some threshold should result the correct lowest point. """
-        pc_in = get_test_data()
-        pc_out = select_below(pc_in, 'z', 3.2)
-        self.assertEqual(len(pc_out['points']['x']['data']), 1)
-        np.testing.assert_almost_equal(pc_out['points']['x']['data'][0], 1.1)
-        np.testing.assert_almost_equal(pc_out['points']['y']['data'][0], 2.1)
-        np.testing.assert_almost_equal(pc_out['points']['z']['data'][0], 3.1)
-        np.testing.assert_almost_equal(pc_out['points']['return']['data'][0], 1)
+def assert_none_key_raises_value_error(function):
+    pc = get_test_data()
+    with pytest.raises(ValueError):
+        function(pc, None, 13)
