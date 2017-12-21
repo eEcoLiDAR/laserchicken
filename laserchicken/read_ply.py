828a5056ae9e67b147d8e7a476497c25d3b668e3
cwmeijer <c.meijer@esciencecenter.nl>
2017-12-21 17:14:41 +0100

    add read properties of ply file

--- laserchicken/read_ply.py
+++ laserchicken/read_ply.py
@@ -5,6 +5,8 @@ import numpy as np
 
 from pytest import raises
 
+from laserchicken import keys
+
 
 def read(path):
     if not os.path.exists(path):
@@ -19,15 +21,38 @@ def read(path):
         if 'ply' not in first_line:
             raise ValueError('Not a valid ply file: {}'.format(path))
 
+        index = []
         for line in ply:
-            index = []
             element_type = 'vertex'
             element_vertex_ = 'element {} '.format(element_type)
             if element_vertex_ in line:
                 number_of_elements = int(line.split(element_vertex_)[1])
-                index.append({'type': element_type, 'number_of_elements': number_of_elements})
+                current_properties = []
+                index.append(
+                    {'type': element_type, 'number_of_elements': number_of_elements, 'properties': current_properties})
 
-            return [[None for i in range(element_block['number_of_elements'])] for element_block in index]
+            if line[:9] == 'property ':
+                property_type, property_name = line[9:].strip('\n').split(' ')
+                current_properties.append({'type': property_type, 'name': property_name})
+
+        return {block['type']: read_block(block, ply) for block in index}
+
+
+def read_block(block, ply_body):
+    properties = {}
+    for prop in block['properties']:
+        if prop['type'] == 'float':
+            dtype = np.float64
+        if prop['type'] == 'int':
+            dtype = np.int32
+        properties[prop['name']] = {'type': prop['type'], 'data': np.zeros(block['number_of_elements'], dtype)}
+    for i in range(block['number_of_elements']):
+        pass
+        # line = ply_body.readline()
+        # values = line.split(' ')
+        # if len(values) != len(block['properties']):
+        #     raise ValueError('Error reading line {} of {} list.'.format(i, block['type']))
+    return properties
 
 
 class TestReadPly(unittest.TestCase):
@@ -51,15 +76,21 @@ class TestReadPly(unittest.TestCase):
 
     def test_containsPointsElement(self):
         data = read(self.test_file_path)
-        self.assertIn('points', data)
+        self.assertIn(keys.point, data)
+
+    def test_containsXElement(self):
+        data = read(self.test_file_path)
+        self.assertIn('x', data[keys.point])
 
     def test_rightNumberOfPoints(self):
         data = read(self.test_file_path)
-        self.assertEqual(len(data['points']), 3)
+        self.assertEqual(len(data[keys.point]['x']['data']), 3)
 
     def test_correctPoints(self):
         data = read(self.test_file_path)
-        np.testing.assert_allclose(data['points'][0]['data'], np.array([0.11, 0.12, 0.13, 1]))
+        points = data[keys.point]
+        point = np.array([points['x']['data'][0], points['y']['data'][0], points['z']['data'][0]])
+        np.testing.assert_allclose(point, np.array([0.11, 0.12, 0.13, 1]))
 
     def setUp(self):
         os.mkdir(self._test_dir)
