6b1a64de053ec0b4cd572415a0e59f3e7ffde497
cwmeijer <c.meijer@esciencecenter.nl>
2018-01-16 15:18:09 +0100

    can read ply

--- laserchicken/read_ply.py
+++ laserchicken/read_ply.py
@@ -21,38 +21,79 @@ def read(path):
         if 'ply' not in first_line:
             raise ValueError('Not a valid ply file: {}'.format(path))
 
-        index = []
-        for line in ply:
-            element_type = 'vertex'
-            element_vertex_ = 'element {} '.format(element_type)
-            if element_vertex_ in line:
-                number_of_elements = int(line.split(element_vertex_)[1])
-                current_properties = []
-                index.append(
-                    {'type': element_type, 'number_of_elements': number_of_elements, 'properties': current_properties})
+        index = read_header(ply)
+        result = {block['type']: read_block(block, ply) for block in index}
+        return result
 
-            if line[:9] == 'property ':
-                property_type, property_name = line[9:].strip('\n').split(' ')
-                current_properties.append({'type': property_type, 'name': property_name})
 
-        return {block['type']: read_block(block, ply) for block in index}
+def read_header(ply):
+    index = []
+    for line in ply:
+        if line[:8] == 'element ':
+            element_type = line.split()[1]
+            number_of_elements = int(line.split()[2])
+            current_properties = []
+            index.append(
+                {'type': element_type, 'number_of_elements': number_of_elements, 'properties': current_properties})
+
+        if line[:9] == 'property ':
+            property_type, property_name = line[9:].strip('\n').split(' ')
+            current_properties.append({'type': property_type, 'name': property_name})
+
+        if line.strip() == 'end_header':
+            break
+    return index
 
 
 def read_block(block, ply_body):
+    properties, property_names = get_properties(block)
+    block_type = block['type']
+    number_of_elements = block['number_of_elements']
+
+    read_elements(ply_body, properties, property_names, block_type, number_of_elements)
+    return properties
+
+
+def cast(value, value_type):
+    if value_type == 'float':
+        dtype = np.float32
+    elif value_type == 'double':
+        dtype = np.float64
+    elif value_type == 'int':
+        dtype = np.int32
+    else:
+        raise ValueError('Invalid type: {}'.format(value_type))
+    return dtype(value)
+
+
+def read_elements(ply_body, properties, property_names, block_type, number_of_elements):
+    for i in range(number_of_elements):
+        line = ply_body.readline()
+        values = line.split(' ')
+        if len(values) != len(property_names):
+            raise ValueError('Error reading line {} of {} list.'.format(i, block_type))
+        for p, property_name in enumerate(property_names):
+            property_type = properties[property_name]['type']
+            value = cast(values[p], property_type)
+            properties[property_name]['data'][i] = value
+
+
+def get_properties(block):
     properties = {}
+    property_names = []
     for prop in block['properties']:
         if prop['type'] == 'float':
+            dtype = np.float32
+        elif prop['type'] == 'double':
             dtype = np.float64
-        if prop['type'] == 'int':
+        elif prop['type'] == 'int':
             dtype = np.int32
-        properties[prop['name']] = {'type': prop['type'], 'data': np.zeros(block['number_of_elements'], dtype)}
-    for i in range(block['number_of_elements']):
-        pass
-        # line = ply_body.readline()
-        # values = line.split(' ')
-        # if len(values) != len(block['properties']):
-        #     raise ValueError('Error reading line {} of {} list.'.format(i, block['type']))
-    return properties
+        else:
+            raise ValueError('Property has no valid type: {}'.format(prop))
+        property_name = prop['name']
+        properties[property_name] = {'type': prop['type'], 'data': np.zeros(block['number_of_elements'], dtype)}
+        property_names.append(property_name)
+    return properties, property_names
 
 
 class TestReadPly(unittest.TestCase):
@@ -89,7 +130,8 @@ class TestReadPly(unittest.TestCase):
     def test_correctPoints(self):
         data = read(self.test_file_path)
         points = data[keys.point]
-        point = np.array([points['x']['data'][0], points['y']['data'][0], points['z']['data'][0]])
+        point = np.array(
+            [points['x']['data'][0], points['y']['data'][0], points['z']['data'][0], points['return']['data'][0]])
         np.testing.assert_allclose(point, np.array([0.11, 0.12, 0.13, 1]))
 
     def setUp(self):
