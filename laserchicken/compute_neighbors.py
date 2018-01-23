3b597ce2e7b6d7bae11743d9f88d743a1e5719c6
elboyran <e.ranguelova@esciencecenter.nl>
2018-01-23 18:16:31 +0100

    small step towards neighbourhood computation...

--- laserchicken/compute_neighbors.py
+++ laserchicken/compute_neighbors.py
@@ -1,1 +1,19 @@
+from laserchicken import kd_tree, keys
 
+def compute_sphere_neighbourhoods(env_pc, target_pc, radius):
+    ''' Function to create a cylindrincal neighbourhood for a given point of
+    a target point cloud among the points from an environment point cloud'''
+    
+    # get the kd tree for the environment point cloud
+    env_tree = kd_tree.get_kdtree_for_pc(env_pc)
+    
+    # get a kd tree for the target point cloud
+    target_tree = kd_tree.get_kdtree_for_pc(target_pc)
+    
+    # compute the points within an 'infinite' cylinder with a given radius
+    neighb_points_indicies = target_tree.query_ball_tree(env_tree, radius)
+    print("neighb_points_indicies", neighb_points_indicies)
+    
+    
+    
+    
---
+++ laserchicken/test_compute_neighbors.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 23 17:09:33 2018
+
+@author: elena
+"""
+
+import os
+import unittest
+import random
+from select import _copy_dict
+from laserchicken import keys, compute_neighbors, read_las
+
+   
+class TestComputeNeighbors(unittest.TestCase):
+
+    _test_file_name = 'AHN3.las'
+    _test_data_source = 'testdata'
+    pointcloud = None
+
+    def test_compute_neighbours(self):
+       ''' Tetsing  computing of neighbors with KDtree '''
+       num_all_pc_points = len(self.pointcloud[keys.point]["x"]["data"])
+       rand_indices = [random.randint(0, num_all_pc_points) for p in range(5)] 
+       print("rand_indicies: ", rand_indices)
+       
+       target_pointcloud = _copy_dict(self.pointcloud, rand_indices)
+      # print("pointcloud: ", self.pointcloud)
+       radius = 0.5
+       
+       compute_neighbors.compute_sphere_neighbourhoods(self.pointcloud, target_pointcloud, radius)
+       raise Exception   
+
+    def setUp(self):
+        self.pointcloud = read_las.read(os.path.join(self._test_data_source,self._test_file_name))
+
+    def tearDown(self):
+        pass
