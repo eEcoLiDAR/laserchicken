42d6badda92a0b475d4377fbc451d9098d3b32e8
Christiaan Meijer <c.meijer@esciencecenter.nl>
2018-01-24 10:15:57 +0100

    fix typo in comment

--- laserchicken/feature_extractor/abc.py
+++ laserchicken/feature_extractor/abc.py
@@ -21,7 +21,7 @@ class AbstractFeatureExtractor(object):
         """
         Get a list of names of the feature values.
 
-        This will return as many names as as the number feature values that will be returned.
+        This will return as many names as the number feature values that will be returned.
         For instance, if a feature extractor returns the first 3 Eigen values, this method
         should return 3 names, for instance 'eigen_value_1', 'eigen_value_2' and 'eigen_value_3'.
 
