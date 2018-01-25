f89d886248027a4b82dba2f8c083093936cc53c9
cwmeijer <c.meijer@esciencecenter.nl>
2018-01-25 13:40:29 +0100

    fix bug in deep copy concerning masks containing only False

--- laserchicken/utils.py
+++ laserchicken/utils.py
@@ -4,7 +4,7 @@ from laserchicken import keys
 def get_point(pc,index):
     return pc[keys.point]["x"]["data"][index],pc[keys.point]["y"]["data"][index],pc[keys.point]["z"]["data"][index]
 
-def copy_pointcloud(pc_in, array_mask = []):
+def copy_pointcloud(pc_in, array_mask = None):
     """
     Makes a deep copy of a point cloud dict using the array mask when copying the points.
     :param pc_in: Input point cloud
@@ -16,7 +16,7 @@ def copy_pointcloud(pc_in, array_mask = []):
         if isinstance(value, dict):
             new_value = copy_pointcloud(value, array_mask)
         elif isinstance(value, np.ndarray):
-            if any(array_mask):
+            if array_mask is not None:
                 new_value = value[array_mask] if any(value) else np.copy(value)
             else:
                 new_value = np.copy(value)
