c38b2b59074ba2ea1df0fda833774e2b438c074d
cwmeijer <c.meijer@esciencecenter.nl>
2018-02-05 10:38:28 +0100

    reformat entropy feature

--- laserchicken/feature_extractor/entropy_feature_extractor.py
+++ laserchicken/feature_extractor/entropy_feature_extractor.py
@@ -1,13 +1,15 @@
+"""Shannan entropy calculation. For more info see https://rdrr.io/cran/lidR/man/entropy.html"""
+
 import numpy as np
 from laserchicken import keys
 from laserchicken.feature_extractor.abc import AbstractFeatureExtractor
 
-class EntropyFeatureExtractor(AbstractFeatureExtractor):
 
+class EntropyFeatureExtractor(AbstractFeatureExtractor):
     # TODO: make this settable from command line
     layer_thickness = 0.5
-    zmin = None
-    zmax = None
+    z_min = None
+    z_max = None
 
     @classmethod
     def requires(cls):
@@ -19,23 +21,22 @@ class EntropyFeatureExtractor(AbstractFeatureExtractor):
 
     def get_params(self):
         p = [self.layer_thickness]
-        if(not self.zmin is None):
-            p.append(self.zmin)
-        if(not self.zmax is None):
-            p.append(self.zmax)
+        if self.z_min is not None:
+            p.append(self.z_min)
+        if self.z_max is not None:
+            p.append(self.z_max)
         return p
 
-
-    def extract(self,sourcepc,neighborhood,targetpc,targetindex):
-        z = sourcepc[keys.point]["z"]["data"][neighborhood]
-        _zmin = np.min(z) if self.zmin is None else self.zmin
-        _zmax = np.max(z) if self.zmax is None else self.zmax
-        nbins = int(np.ceil((_zmax - _zmin)/self.layer_thickness))
-        data = np.histogram(z,bins = nbins,range = (_zmin,_zmax),density = True)[0]
-        entropyfunc = np.vectorize(xlog2x)
+    def extract(self, source_pc, neighborhood, target_pc, target_index):
+        z = source_pc[keys.point]["z"]["data"][neighborhood]
+        _z_min = np.min(z) if self.z_min is None else self.z_min
+        _z_max = np.max(z) if self.z_max is None else self.z_max
+        n_bins = int(np.ceil((_z_max - _z_min) / self.layer_thickness))
+        data = np.histogram(z, bins=n_bins, range=(_z_min, _z_max), density=True)[0]
+        entropy_func = np.vectorize(_x_log_2x)
         norm = np.sum(data)
-        return -(entropyfunc(data/norm)).sum()
+        return -(entropy_func(data / norm)).sum()
 
 
-def xlog2x(x):
+def _x_log_2x(x):
     return 0 if x == 0 else x * np.log2(x)
