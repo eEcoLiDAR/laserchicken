4d5dfa9647bc98a0ce3b83e7aae792bcb396785b
cwmeijer <c.meijer@esciencecenter.nl>
2018-02-05 10:45:05 +0100

    reformat eigen values feature

--- laserchicken/feature_extractor/test_eigenvals_feature_extractor.py
+++ laserchicken/feature_extractor/test_eigenvals_feature_extractor.py
@@ -9,27 +9,27 @@ class TestExtractEigenValues(unittest.TestCase):
 
     _test_file_name = 'AHN3.las'
     _test_data_source = 'testdata'
-    pointcloud = None
+    point_cloud = None
 
     def test_eigenvalues_in_cylinders(self):
         """Test computing of eigenvalues in cylinder."""
-        num_all_pc_points = len(self.pointcloud[keys.point]["x"]["data"])
+        num_all_pc_points = len(self.point_cloud[keys.point]["x"]["data"])
         rand_indices = [random.randint(0, num_all_pc_points) for p in range(20)]
-        target_pointcloud = utils.copy_pointcloud(self.pointcloud, rand_indices)
+        target_pointcloud = utils.copy_pointcloud(self.point_cloud, rand_indices)
         numtargets = len(target_pointcloud[keys.point]["x"]["data"])
         radius = 2.5
         result_index_lists = compute_neighbors.compute_cylinder_neighbourhood_indicies(
-            self.pointcloud, target_pointcloud, radius)
-        feature_extractor.compute_features(self.pointcloud, result_index_lists, target_pointcloud,
+            self.point_cloud, target_pointcloud, radius)
+        feature_extractor.compute_features(self.point_cloud, result_index_lists, target_pointcloud,
                                            ["eigenv_1", "eigenv_2", "eigenv_3"])
         for i in range(numtargets):
             lambda1, lambda2, lambda3 = utils.get_features(target_pointcloud, i, ["eigenv_1", "eigenv_2", "eigenv_3"])
-            self.assertTrue(lambda1 >= lambda2 and lambda2 >= lambda3)
+            self.assertTrue(lambda1 >= lambda2 >= lambda3)
         self.assertEqual("laserchicken.feature_extractor.eigenvals_feature_extractor",
                          target_pointcloud[keys.provenance][0]["module"])
 
     def setUp(self):
-        self.pointcloud = read_las.read(os.path.join(self._test_data_source, self._test_file_name))
+        self.point_cloud = read_las.read(os.path.join(self._test_data_source, self._test_file_name))
         random.seed(102938482634)
 
     def tearDown(self):
