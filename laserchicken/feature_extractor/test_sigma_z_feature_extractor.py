48ccb3185cc4f3d82722087d8b652975cf1d69e8
cwmeijer <c.meijer@esciencecenter.nl>
2018-02-14 15:03:20 +0100

    add sigma z feature extractor, Fixes #20

---
+++ laserchicken/feature_extractor/test_sigma_z_feature_extractor.py
@@ -0,0 +1,51 @@
+import unittest
+
+import numpy as np
+
+from laserchicken import keys
+from laserchicken.feature_extractor import compute_features
+from laserchicken.test_tools import create_point_cloud
+from laserchicken.volume_specification import InfiniteCylinder
+
+
+class TestExtractSigmaZ(unittest.TestCase):
+    def test_constantValues_result0(self):
+        def z_constant(x, y):
+            return 2
+
+        assert_std_for_z_function_in_xy_grid(z_constant, 0)
+
+    def test_checkered1sAnd0s_resultHalf(self):
+        """Standard deviation of checker pattern of 0s and 1s should be 0.5"""
+
+        def z_checkered(x, y):
+            return ((x + y) % 2) + 2
+
+        assert_std_for_z_function_in_xy_grid(z_checkered, 0.5)
+
+    def test_checkered1sAnd0sPlusSkewed_resultHalf(self):
+        """Standard deviation of checker pattern of 0s and 1s should be 0.5, adding a plane should not change that"""
+
+        def z_checkered(x, y):
+            return ((x + y) % 2) + 2 + x + y
+
+        assert_std_for_z_function_in_xy_grid(z_checkered, 0.5)
+
+
+def assert_std_for_z_function_in_xy_grid(z_checkered, expected):
+    """Assert that the standard deviation of z values in a grid of unit x and y"""
+    n_points, points = create_points_in_xy_grid(z_checkered)
+    point_cloud = create_point_cloud(points[:, 0], points[:, 1], points[:, 2])
+    targets = create_point_cloud([0], [0], [0])
+    compute_features(point_cloud, [range(n_points)], targets, ['sigma_z'], InfiniteCylinder(10))
+    np.testing.assert_almost_equal(targets[keys.point]['sigma_z']['data'][0], expected)
+
+
+def create_points_in_xy_grid(z_function):
+    n_points = 100
+    points = np.zeros((n_points, 3))
+    for i in range(n_points):
+        x = i % np.sqrt(n_points)
+        y = np.floor(i / np.sqrt(n_points))
+        points[i] = np.array((x, y, z_function(x, y)))
+    return n_points, points
