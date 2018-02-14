0779d5ac8aae309230af4afcfb3a06f2b0e3794e
Nicolas Renaud <n.renaud@tudelft.nl>
2018-02-14 15:25:08 +0100

    fixed syntax

--- laserchicken/feature_extractor/test_normal_plane_feature_extractor.py
+++ laserchicken/feature_extractor/test_normal_plane_feature_extractor.py
@@ -1,32 +1,32 @@
-import numpy as np
 import unittest
-from laserchicken import keys,utils
+import numpy as np
+from laserchicken import keys, utils
 from laserchicken.feature_extractor.normal_plane_feature_extractor import NormalPlaneFeatureExtractor
 
 
 class TestNormalPlaneFeatureExtractor(unittest.TestCase):
+    """Test the normal plane extractor."""
 
     pc = None
     neighborhood = None
     nvect = None
 
     def test_normal_plane(self):
-
+        """Do the test."""
         extractor = NormalPlaneFeatureExtractor()
-        nfit = extractor.extract(self.pc,self.neighborhood,None,None,None)
-        self.assertTrue(np.allclose(self.nvect,nfit))
+        nfit = extractor.extract(self.pc, self.neighborhood, None, None, None)
+        self.assertTrue(np.allclose(self.nvect, nfit))
 
     def setUp(self):
-        self.nvect = np.array([1.,2.,3.])
+        """Set up of the test."""
+        self.nvect = np.array([1., 2., 3.])
         self.nvect /= np.linalg.norm(self.nvect)
-        x,y,z = utils.generate_random_points_inplane(self.nvect,npts=10)
+        x, y, z = utils.generate_random_points_inplane(self.nvect, npts=10)
         self.pc = {keys.point: {'x': {'type': 'double', 'data': x},
                            'y': {'type': 'double', 'data': y},
                            'z': {'type': 'double', 'data': z}}}
-        self.neighborhood = [3,4,5,6,7]
+        self.neighborhood = [3, 4, 5, 6, 7]
 
     def tearDown(self):
+        """Tear it down."""
         pass
-
-if __name__ == '__main__':
-    unittest.main()
