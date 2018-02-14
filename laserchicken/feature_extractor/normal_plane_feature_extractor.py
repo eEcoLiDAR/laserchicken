76b7a1f98a91fad91ce349e74e5de26a17b9cd7f
Nicolas Renaud <n.renaud@tudelft.nl>
2018-02-14 14:15:05 +0100

    normal plane feature added

---
+++ laserchicken/feature_extractor/normal_plane_feature_extractor.py
@@ -0,0 +1,60 @@
+from laserchicken.feature_extractor.abc import AbstractFeatureExtractor
+from laserchicken.keys import point
+from laserchicken.utils import fit_plane_svd
+
+class NormalPlaneFeatureExtractor(AbstractFeatureExtractor):
+    """Feature extractor for the normal of a plane."""
+
+    @classmethod
+    def requires(cls):
+        """
+        Get a list of names of the point attributes that are needed for this feature extraction.
+
+        For simple features, this could be just x, y, and z. Other features can build on again
+        other features to have been computed first.
+
+        :return: List of feature names
+        """
+        return []
+
+    @classmethod
+    def provides(cls):
+        """
+        Get a list of names of the feature values.
+
+        This will return as many names as the number feature values that will be returned.
+        For instance, if a feature extractor returns the first 3 Eigen values, this method
+        should return 3 names, for instance 'eigen_value_1', 'eigen_value_2' and 'eigen_value_3'.
+
+        :return: List of feature names
+        """
+        return ['normal_vector']
+
+    def extract(self, sourcepc, neighborhood, targetpc, targetindex, volume):
+
+        """
+        Extract the feature value(s) of the point cloud at location of the target.
+
+        :param point_cloud: environment (search space) point cloud
+        :param neighborhood: array of indices of points within the point_cloud argument
+        :param target_point_cloud: point cloud that contains target point
+        :param target_index: index of the target point in the target pointcloud
+        :param volume_description: volume object that describes the shape and size of the search volume
+        :return: feature value
+        """
+
+        x = sourcepc[point]['data']['x'][neighborhood]
+        y = sourcepc[point]['data']['y'][neighborhood]
+        z = sourcepc[point]['data']['z'][neighborhood]
+
+        nvect = fit_plane_svd(x,y,z)
+
+        return nvect
+
+    def get_params(self):
+        """
+        Return a tuple of parameters involved in the current feature extractorobject.
+
+        Needed for provenance.
+        """
+        return ()
---
+++ laserchicken/feature_extractor/test_normal_plane_feature_extractor.py
@@ -0,0 +1,30 @@
+import numpy as np
+import unittest
+from laserchicken import keys,utils
+from laserchicken.feature_extractor.normal_plane_feature_extractor import NormalPlaneFeatureExtractor
+
+
+class TestNormalPlaneFeatureExtractor(unittest.TestCase):
+
+    pc = None
+    neighborhood = None
+    nvect = None
+
+    def test_normal_plane(self):
+
+        extractor = NormalPlaneFeatureExtractor()
+        nfit = extractor.extract(self.pc,self.neighborhood,None,None,None)
+        self.assertTrue(np.allclose(self.nvect,nfit))
+
+    def setUp(self):
+        self.nvect = np.array([1.,2.,3.])
+        self.nvect /= np.linalg.norm(self.nvect)
+        x,y,z = utils.generate_random_points_inplane(self.nvect,npts=10)
+        self.pc = {keys.point: {'data': {'x': x,'y':y,'z':z} } }
+        self.neighborhood = [3,4,5,6,7]
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
