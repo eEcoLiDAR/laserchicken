f79bbc396d5c91f23fb4a6bd00a95227e2edc229
Nicolas Renaud <n.renaud@tudelft.nl>
2018-02-19 10:55:05 +0100

    controlled test

--- laserchicken/feature_extractor/test_echo_ratio_extractor.py
+++ laserchicken/feature_extractor/test_echo_ratio_extractor.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import pytest
 import itertools
 import random
 import numpy as np
@@ -18,36 +19,82 @@ class TestEchoRatioFeatureExtractor(unittest.TestCase):
     targetpc = None
 
     def test_(self):
+        """Must pass as we provide everything needed."""
 
-        index_sphere = compute_neighborhoods(self.point_cloud, self.targetpc, Sphere(self.radius))
-        index_cyl = compute_neighborhoods(self.point_cloud, self.targetpc, InfiniteCylinder(self.radius))
+        # radius must be slightly larger than radius to avoid rounding error
+        index_sphere = compute_neighborhoods(self.point_cloud, self.targetpc, Sphere(self.radius+1E-3))
+        index_cyl = compute_neighborhoods(self.point_cloud, self.targetpc, InfiniteCylinder(self.radius+1E-3))
         indexes = [index_sphere[0],index_cyl[0]]
 
         extractor = EchoRatioFeatureExtractor()
         per = extractor.extract(self.point_cloud, indexes, None, None, None)
-        print(per)
-        #self.assertTrue(np.allclose(per,test_values))
 
-    def _get_data(self):
-        """Create a 3D grid of equally spaved points."""
+        self.assertTrue(np.allclose(per,self.theo_val))
 
-        x = np.linspace(-1, 1, 11)
-        xyz = np.array([ list(p) for p in list(itertools.product(x,repeat=3))])
+    def test_invalid(self):
+        """ Must raise ValueError as we do not provide correct indexes."""
+
+        extractor = EchoRatioFeatureExtractor()
+        with pytest.raises(ValueError):
+            extractor.extract(self.point_cloud, [1,2,3], None,None,None)
+
+    def _make_pc(self,xyz):
         return {keys.point: {'x': {'type': 'double', 'data': xyz[:, 0]},
-                           'y': {'type': 'double', 'data': xyz[:, 1]},
-                           'z': {'type': 'double', 'data': xyz[:, 2]}}},len(xyz)
+                                   'y': {'type': 'double', 'data': xyz[:, 1]},
+                                   'z': {'type': 'double', 'data': xyz[:, 2]}}}, len(xyz)
 
-    def _get_central_point(self):
+    def _get_sphere(self):
+        """Create a sphere of point."""
+
+        nteta,nphi = 11,11
+        teta = np.linspace(0.1,2*np.pi,nteta)
+        phi = np.linspace(0.1,np.pi,nphi)
+        r = self.radius
+        for t in teta:
+            for p in phi:
+                x = r * np.cos(t) * np.sin(p)
+                y = r * np.sin(t) * np.sin(p)
+                z = r * np.cos(p)
+                self.xyz.append([x, y, z])
+        return nteta * nphi
+
+    def _get_cylinder(self):
+        """Create a cylinder of point."""
+
+        # nheight must be even so that the cylinder has no point
+        # on the equator of the sphere
+        nteta,nheight = 11,10
+        r = self.radius
+        teta = np.linspace(0.1, 2 * np.pi, nteta)
+        height = np.linspace(-2*r, 2*r, nheight)
+
+        for h in height:
+            for t in teta:
+                x,y,z = r * np.cos(t), r * np.sin(t), h
+                self.xyz.append([x, y, z])
+        return nteta * nheight
+
+    def _get_central_point(self,index):
         """Get the central point."""
-        return utils.copy_pointcloud(self.point_cloud, [int(self.npts/2)])
+        return utils.copy_pointcloud(self.point_cloud, [index])
 
 
     def setUp(self):
-        """Set up the test."""
+        """
+        Set up the test.
 
-        self.point_cloud,self.npts = self._get_data()
-        self.targetpc = self._get_central_point()
+        Create a sphere and a cylinder of points and a central point
+        The cylinder has no points on the equator of the sphere
+        """
+
+        # radius pf the cylinder and sphere
         self.radius = 0.5
+        self.xyz = [[0.,0.,0.]]
+        self.npt_sphere = self._get_sphere()
+        self.npt_cyl = self._get_cylinder()
+        self.point_cloud,self.npts = self._make_pc(np.array(self.xyz))
+        self.targetpc = self._get_central_point(0)
+        self.theo_val = (self.npt_sphere + 1) / (self.npt_sphere + self.npt_cyl + 1) * 100
 
     def tearDowm(self):
         """Tear it down."""
