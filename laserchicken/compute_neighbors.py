a6d645c90eb1efb710aff638dc68817529a553be
romulogoncalves <r.goncalves@esciencecenter.nl>
2018-03-28 16:37:06 +0200

    compute_neighbors is now a generator.

--- laserchicken/compute_neighbors.py
+++ laserchicken/compute_neighbors.py
@@ -18,7 +18,7 @@ def frange(x, y, jump):
 MEMORY_THRESHOLD = 0.5
 POINTCLOUD_DIST = 10
 
-def compute_cylinder_neighborhood_(environment_pc, target_pc, radius):
+def compute_cylinder_neighborhood(environment_pc, target_pc, radius):
     """Find the indices of points within a cylindrical neighbourhood (using KD Tree)
     for a given point of a target point cloud among the points from an environment point cloud.
 
@@ -60,26 +60,7 @@ def compute_cylinder_neighborhood_(environment_pc, target_pc, radius):
       yield target_tree.query_ball_tree(env_tree, radius)
 
 
-def compute_cylinder_neighborhood(environment_pc, target_pc, radius):
-    """Find the indices of points within a cylindrical neighbourhood (using KD Tree)
-    for a given point of a target point cloud among the points from an environment point cloud.
-
-    :param environment_pc: environment point cloud
-    :param target_pc: point cloud that contains the points at which neighborhoods are to be calculated
-    :param radius: search radius for neighbors
-    :return: indices of neighboring points from the environment point cloud for each target point
-             the returned indices also contains the index of the target point.
-    """
-
-    print("Start tree creation")
-    env_tree = kd_tree.get_kdtree_for_pc(environment_pc)
-    print("Done with env tree creation")
-    target_tree = kd_tree.get_kdtree_for_pc(target_pc)
-    print("Done with target tree creation")
-    return target_tree.query_ball_tree(env_tree, radius)
-
-
-def compute_sphere_neighborhood_(environment_pc, target_pc, radius):
+def compute_sphere_neighborhood(environment_pc, target_pc, radius):
     """
     Find the indices of points within a spherical neighbourhood for a given point of a target point cloud among
     the points from an environment point cloud.
@@ -90,7 +71,7 @@ def compute_sphere_neighborhood_(environment_pc, target_pc, radius):
     :return: indices of neighboring points from the environment point cloud for each target point
     """
 
-    neighbors = compute_cylinder_neighborhood_(environment_pc, target_pc, radius)
+    neighbors = compute_cylinder_neighborhood(environment_pc, target_pc, radius)
 
     for neighborhood_indices in neighbors:
       result = []
@@ -108,57 +89,6 @@ def compute_sphere_neighborhood_(environment_pc, target_pc, radius):
       yield result
 
 
-def compute_sphere_neighborhood(environment_pc, target_pc, radius):
-    """
-    Find the indices of points within a spherical neighbourhood for a given point of a target point cloud among
-    the points from an environment point cloud.
-
-    :param environment_pc: environment point cloud
-    :param target_pc: point cloud that contains the points at which neighborhoods are to be calculated
-    :param radius: search radius for neighbors
-    :return: indices of neighboring points from the environment point cloud for each target point
-    """
-
-    neighborhood_indices = compute_cylinder_neighborhood_(environment_pc, target_pc, radius)
-
-    result = []
-    for i in range(len(neighborhood_indices)):
-      target_x, target_y, target_z = utils.get_point(target_pc, i)
-      neighbor_indices = neighborhood_indices[i]
-      result_indices = []
-      for j in neighbor_indices:
-        env_x, env_y, env_z = utils.get_point(environment_pc, j)
-        if abs(target_z - env_z) > radius:
-          continue
-        if (env_x - target_x) ** 2 + (env_y - target_y) ** 2 + (env_z - target_z) ** 2 <=  radius ** 2 :
-          result_indices.append(j)
-      result.append(result_indices)
-    return result
-
-
-def compute_neighborhoods_(env_pc, target_pc, volume_description):
-    """
-    Find a subset of points in a neighbourhood in the environment point cloud for each point in a target point cloud.
-
-    :param env_pc: environment point cloud
-    :param target_pc: point cloud that contains the points at which neighborhoods are to be calculated
-    :param volume_description: volume object that describes the shape and size of the search volume
-    :return: indices of neighboring points from the environment point cloud for each target point
-    """
-    volume_type = volume_description.get_type()
-    if volume_type == Sphere.TYPE:
-      compute_neighborhoods = compute_sphere_neighborhood_(env_pc, target_pc, volume_description.radius)
-    elif volume_type == InfiniteCylinder.TYPE:
-      compute_neighborhoods = compute_cylinder_neighborhood_(env_pc, target_pc, volume_description.radius)
-    else:
-      raise ValueError('Neighborhood computation error because volume type "{}" is unknown.'.format(volume_type))
-    print ("Start yielding neighbors!!!")
-    for x in compute_neighborhoods:
-      print("Size of x: %d" % len(x))
-      print(x[:10])
-      yield x
-
-
 def compute_neighborhoods(env_pc, target_pc, volume_description):
     """
     Find a subset of points in a neighbourhood in the environment point cloud for each point in a target point cloud.
@@ -173,5 +103,10 @@ def compute_neighborhoods(env_pc, target_pc, volume_description):
       compute_neighborhoods = compute_sphere_neighborhood(env_pc, target_pc, volume_description.radius)
     elif volume_type == InfiniteCylinder.TYPE:
       compute_neighborhoods = compute_cylinder_neighborhood(env_pc, target_pc, volume_description.radius)
-    return compute_neighborhoods
-    raise ValueError('Neighborhood computation error because volume type "{}" is unknown.'.format(volume_type))
+    else:
+      raise ValueError('Neighborhood computation error because volume type "{}" is unknown.'.format(volume_type))
+    print ("Start yielding neighbors!!!")
+    for x in compute_neighborhoods:
+      print("Size of x: %d" % len(x))
+      print(x[:10])
+      yield x
