cf55ff1d067e8fe7e580f6a360ef104f83cc299a
cwmeijer <c.meijer@esciencecenter.nl>
2018-04-25 13:40:42 +0200

    add bunch of generic tests all features should pass

--- laserchicken/feature_extractor/test_all_features.py
+++ laserchicken/feature_extractor/test_all_features.py
@@ -8,6 +8,7 @@ import pytest
 from laserchicken import compute_neighbors
 from laserchicken import keys
 from laserchicken import read_las
+from laserchicken.keys import point
 from laserchicken.utils import copy_pointcloud
 from laserchicken.volume_specification import InfiniteCylinder
 from . import _feature_map
@@ -27,16 +28,90 @@ _pc_10 = copy_pointcloud(_pc_260807, array_mask=(
     np.random.choice(range(len(_pc_260807[keys.point]['x']['data'])), size=10, replace=False)))
 _1000_neighborhoods_in_260807 = next(compute_neighbors.compute_neighborhoods(_pc_260807, _pc_1000, _cylinder))
 _10_neighborhoods_in_260807 = next(compute_neighbors.compute_neighborhoods(_pc_260807, _pc_10, _cylinder))
+_260807_neighborhoods_in_10 = next(compute_neighbors.compute_neighborhoods(_pc_10, _pc_260807, _cylinder))
+
+feature_names = [name for name in _feature_map()]
 
 
-@pytest.mark.parametrize("feature", [name for name in _feature_map()])
-def test_completeTile_noErrors(feature):
-    compute_features(copy_pointcloud(_pc_260807), _1000_neighborhoods_in_260807, 0, copy_pointcloud(_pc_1000),
-                     [feature],
-                     volume=_cylinder)
+@pytest.mark.parametrize("feature", feature_names)
+def test_completeTile_consistentOutput(feature):
+    target_point_cloud = copy_pointcloud(_pc_1000)
+    compute_features(copy_pointcloud(_pc_260807), _1000_neighborhoods_in_260807, 0, target_point_cloud,
+                     [feature], volume=_cylinder)
+    _assert_consistent_attribute_length(target_point_cloud)
 
 
-@pytest.mark.parametrize("feature", [name for name in _feature_map()])
+@pytest.mark.parametrize("feature", feature_names)
+def test_manyTargets_consistentOutput(feature):
+    target_point_cloud = copy_pointcloud(_pc_260807)
+    compute_features(copy_pointcloud(_pc_10), _260807_neighborhoods_in_10, 0, target_point_cloud,
+                     [feature], volume=_cylinder)
+    _assert_consistent_attribute_length(target_point_cloud)
+
+
+@pytest.mark.parametrize("feature", feature_names)
+def test_xAllZeros_consistentOutput(feature):
+    n = 10
+    pc = _create_point_cloud(x=0, n=n)
+    compute_features(pc, range(n), 0, pc, [feature], volume=_cylinder)
+    _assert_consistent_attribute_length(pc)
+
+
+@pytest.mark.parametrize("feature", feature_names)
+def test_yAllZeros_consistentOutput(feature):
+    n = 10
+    pc = _create_point_cloud(y=0, n=n)
+    compute_features(pc, range(n), 0, pc, [feature], volume=_cylinder)
+    _assert_consistent_attribute_length(pc)
+
+
+@pytest.mark.parametrize("feature", feature_names)
+def test_zAllZeros_consistentOutput(feature):
+    n = 10
+    pc = _create_point_cloud(z=0, n=n)
+    compute_features(pc, range(n), 0, pc, [feature], volume=_cylinder)
+    _assert_consistent_attribute_length(pc)
+
+
+@pytest.mark.parametrize("feature", feature_names)
+def test_zeroPoints_consistentOutput(feature):
+    n = 0
+    pc = _create_point_cloud(n=n)
+    compute_features(pc, [[] for _ in range(n)], 0, pc, [feature], volume=_cylinder)
+    _assert_consistent_attribute_length(pc)
+
+
+@pytest.mark.parametrize("feature", feature_names)
+def test_zeroNeighbors_consistentOutput(feature):
+    _test_consistent_output_with_n_neighbors(feature, 0)
+
+
+@pytest.mark.parametrize("feature", feature_names)
+def test_oneNeighbor_consistentOutput(feature):
+    _test_consistent_output_with_n_neighbors(feature, 1)
+
+
+@pytest.mark.parametrize("feature", feature_names)
+def test_twoNeighbors_consistentOutput(feature):
+    _test_consistent_output_with_n_neighbors(feature, 2)
+
+
+def _test_consistent_output_with_n_neighbors(feature, n_neighbors):
+    n_points = 10
+    pc = _create_point_cloud(n=n_points)
+    compute_features(pc, [range(n_neighbors) for _ in range(n_points)], 0, pc, [feature], volume=_cylinder)
+    _assert_consistent_attribute_length(pc)
+
+
+@pytest.mark.parametrize("feature", feature_names)
+def test_oneNeighbor_consistentOutput(feature):
+    n = 1
+    pc = _create_point_cloud(n=n)
+    compute_features(pc, [[] for _ in range(n)], 0, pc, [feature], volume=_cylinder)
+    _assert_consistent_attribute_length(pc)
+
+
+@pytest.mark.parametrize("feature", feature_names)
 def test_inputNotChanged(feature):
     original_environment = _pc_260807
     environment = copy_pointcloud(original_environment)
@@ -48,12 +123,26 @@ def test_inputNotChanged(feature):
     compute_features(environment, neighborhoods, 0, targets, [feature],
                      volume=_cylinder)
 
-    assert_attributes_not_changed(original_environment, environment)
-    assert_attributes_not_changed(original_targets, targets)
+    _assert_attributes_not_changed(original_environment, environment)
+    _assert_attributes_not_changed(original_targets, targets)
     assert json.dumps(original_neighborhoods) == json.dumps(neighborhoods)
 
 
-def assert_attributes_not_changed(original_point_cloud, new_point_cloud):
+def _create_point_cloud(x=None, y=None, z=None, n=10):
+    pc = {point: {'x': {'data': np.array([x if x is not None else i for i in range(n)]), 'type': 'float'},
+                  'y': {'data': np.array([y if y is not None else i for i in range(n)]), 'type': 'float'},
+                  'z': {'data': np.array([z if z is not None else i for i in range(n)]), 'type': 'float'}}}
+    return pc
+
+
+def _assert_attributes_not_changed(original_point_cloud, new_point_cloud):
     for attribute in original_point_cloud[keys.point]:
         np.testing.assert_array_almost_equal(new_point_cloud[keys.point][attribute]['data'],
                                              original_point_cloud[keys.point][attribute]['data'])
+
+
+def _assert_consistent_attribute_length(target_point_cloud):
+    n_elements = len(target_point_cloud[keys.point]['x'])
+    for key in target_point_cloud[keys.point]:
+        print(key, len(target_point_cloud[keys.point][key]))
+        assert n_elements == len(target_point_cloud[keys.point][key])
