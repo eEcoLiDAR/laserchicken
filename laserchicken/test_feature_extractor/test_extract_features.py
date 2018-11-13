f2d58d9c47e981e1e69b77225a7ac5ff30b1ce49
cwmeijer <c.meijer@esciencecenter.nl>
2018-11-13 11:52:38 +0100

    fix typo wich cause one test to skip

--- laserchicken/test_feature_extractor/test_extract_features.py
+++ laserchicken/test_feature_extractor/test_extract_features.py
@@ -16,8 +16,8 @@ class TestExtractFeatures(unittest.TestCase):
     def test_extract_single_feature_ends_up_in_pc():
         target = test_tools.ComplexTestData().get_point_cloud()
         _compute_features(target, ['test3_a'])
-        assert all(target[keys.point]['test3_a']['data'] == target[keys.point]['z']['data']) \
- \
+        assert all(target[keys.point]['test3_a']['data'] == target[keys.point]['z']['data'])
+
     @staticmethod
     def test_extract_only_requested_feature_ends_up_in_pc():
         target = test_tools.ComplexTestData().get_point_cloud()
