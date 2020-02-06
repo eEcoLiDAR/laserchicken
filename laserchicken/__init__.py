bf6c7df30a411c3f1e0ded3a8a91b1f7036fe78c
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-02-06 10:02:39 +0100

    Relevant functions imported in main init
    One can then import them directly from laserchicken

--- laserchicken/__init__.py
+++ laserchicken/__init__.py
@@ -1,1 +1,6 @@
 from ._version import __version__
+
+from laserchicken.compute_neighbors import compute_neighborhoods
+from laserchicken.feature_extractor.features import compute_features
+from laserchicken.io.load import load
+from laserchicken.io.export import export
