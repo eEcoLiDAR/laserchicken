ba5e2242274f3033c9a41171c34eacc4ec7b9f97
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-02-07 17:15:24 +0100

    also feature adder available from laerchicken base

--- laserchicken/__init__.py
+++ laserchicken/__init__.py
@@ -1,6 +1,6 @@
 from ._version import __version__
 
 from laserchicken.compute_neighbors import compute_neighborhoods
-from laserchicken.feature_extractor.features import compute_features
+from laserchicken.feature_extractor.features import compute_features, register_new_feature_extractor
 from laserchicken.io.load import load
 from laserchicken.io.export import export
