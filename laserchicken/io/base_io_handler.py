73277022e89280445f89fd4737ae4fbb7d14b893
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-02-07 09:14:26 +0100

    replace exceptions with specific oserrors in IOHandler

--- laserchicken/io/base_io_handler.py
+++ laserchicken/io/base_io_handler.py
@@ -17,15 +17,15 @@ class IOHandler(object):
         self.path = path
         if mode == 'r':
             if not os.path.exists(path):
-                raise Exception('{} not found.'.format(path))
+                raise FileNotFoundError('{} not found.'.format(path))
         elif mode == 'w':
             path_directory = os.path.dirname(path)
             if not os.path.exists(path_directory):
-                raise Exception('Output file path does not exist! --> {}'.format(path))
+                raise FileNotFoundError('Output file path does not exist! --> {}'.format(path))
             if not overwrite:
                 if os.path.exists(path):
                     # Raise most specific subclass of FileExistsError (3.6) and IOError (2.7).
-                    raise Exception('Output file already exists! --> {}'.format(path))
+                    raise FileExistsError('Output file already exists! --> {}'.format(path))
 
     def read(self):
         """
