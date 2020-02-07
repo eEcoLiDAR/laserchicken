620d438bce0beffb26fe6c54a52368764e8e5c78
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-02-07 09:47:43 +0100

    point_cloud data structure info moved to base_io_handler

--- laserchicken/io/read_las.py
+++ laserchicken/io/read_las.py
@@ -8,15 +8,6 @@ def read(path):
     """
     Load the points from a LAS file into memory.
 
-    Resulting data structure looks like:
-    {'log': ['Processed by module load', 'Processed by module filter using parameters(x,y,z)'],
-     'pointcloud':
-       {'offset': {'type': 'double', 'data': 12.1}},
-     'vertex':
-       {'x': {'type': 'double', 'data': np.array([0.1, 0.2, 0.3])},
-        'y': {'type': 'double', 'data': np.array([0.1, 0.2, 0.3])},
-        'z': {'type': 'double', 'data': np.array([0.1, 0.2, 0.3])},
-        'return': {'type': 'int', 'data': np.array([1, 1, 2])}}}
     :param path: is the path to the las file
     :return: point cloud data structure
     """
