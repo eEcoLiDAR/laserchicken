d4ca0f81396cf3ff6be59d7da13b2a2680b10bd9
Francesco Nattino <f.nattino@esciencecenter.nl>
2020-02-11 17:07:38 +0100

    LAS(LAZ) writer using pylas implemented
    tests have been also added

--- laserchicken/io/las_handler.py
+++ laserchicken/io/las_handler.py
@@ -1,5 +1,12 @@
 """ IO Handler for LAS (and compressed LAZ) file format """
-import laspy
+import numpy as np
+
+_is_pylas_available = False
+try:
+    _is_pylas_available = True
+    import pylas
+except ImportError:
+    import laspy
 
 from laserchicken import keys
 from laserchicken.io.base_io_handler import IOHandler
@@ -14,7 +21,11 @@ class LASHandler(IOHandler):
 
         :return: point cloud data structure
         """
-        file = laspy.file.File(self.path)
+        if _is_pylas_available:
+            file = pylas.read(self.path)
+        else:
+            file = laspy.file.File(self.path)
+
         attributes = {
             'x',
             'y',
@@ -31,6 +42,60 @@ class LASHandler(IOHandler):
 
         return {keys.point: points}
 
+    def write(self, point_cloud, file_version='1.4', point_format_id=1):
+        """
+        Write point cloud to a LAS(LAZ) file.
+
+        :param point_cloud:
+        :param file_version:
+        :param point_format_id:
+        :return:
+        """
+        if not _is_pylas_available:
+            raise NotImplementedError('Writing LAS/LAZ files only available through pylas: '
+                                      'https://pylas.readthedocs.io')
+
+        file = pylas.create(point_format_id=point_format_id,
+                            file_version=file_version)
+
+        points = point_cloud[keys.point]
+
+        # NOTE: adding extra dims and assignment should be done in two steps,
+        # some fields (e.g. raw_classification) are otherwise overwritten
+        for name, attribute in points.items():
+            data, dtype = _get_data_and_type(attribute)
+            if name not in 'xyz':
+                # x,y,z are not there but file methods can be used to convert coords to int4
+                if name not in file.points.dtype.names:
+                    dtype_str = "".join([c for c in dtype.str
+                                         if c not in ['<', '>', '|']])
+                    file.add_extra_dim(name=name, type=dtype_str)
+            file_dtype = getattr(file, name).dtype
+            if not file_dtype.name == dtype.name:
+                raise TypeError('Open file format data type does not match point cloud: '
+                                'for {}, {} vs {}'.format(name, file_dtype.name, dtype.name))
+
+        for name, attribute in points.items():
+            data, _ = _get_data_and_type(attribute)
+            if data.size == 0:
+                raise ValueError('Cannot write empty point-cloud!')
+            else:
+                setattr(file, name, data)
+
+        file.write(self.path)
+
 
 def _get_attribute(data, data_type):
     return {'type': data_type, 'data': data}
+
+
+def _get_data_and_type(attribute):
+    return attribute['data'], np.dtype(attribute['type'])
+
+
+if __name__ == '__main__':
+    from laserchicken import load, export
+    from laserchicken.normalize import normalize
+    pc = load('testdata/AHN3.las')
+    normalize(pc)
+    export(pc, 'testdata/tmp.las', overwrite=True)
---
+++ laserchicken/io/test_write_las.py
@@ -0,0 +1,98 @@
+import os
+import shutil
+import unittest
+
+import laspy
+import numpy as np
+import pytest
+
+from laserchicken import keys
+from laserchicken.test_tools import SimpleTestData, ComplexTestData
+from laserchicken.test_tools import create_point_cloud
+from laserchicken.io.export import export
+from laserchicken.io.load import load
+
+
+class TestWriteLas(unittest.TestCase):
+    _test_dir = 'TestLoad_dir'
+    _test_file_name = 'test.las'
+    _test_data_source = 'testdata'
+    test_file_path = os.path.join(_test_dir, _test_file_name)
+
+    def test_write_emptyData(self):
+        """ Should raise an exception """
+        pc = create_point_cloud([], [], [])
+        with pytest.raises(ValueError):
+            export(pc, self.test_file_path)
+
+    def test_write_nonExistentDir(self):
+        """ Should raise an exception """
+        pc = SimpleTestData.get_point_cloud()
+        with pytest.raises(FileNotFoundError):
+            export(pc, os.path.join('/nonexistentdir', self._test_file_name))
+
+    def test_write_sameFileTwice(self):
+        """ Should raise an exception """
+        pc = SimpleTestData.get_point_cloud()
+        export(pc, self.test_file_path)
+        with pytest.raises(FileExistsError):
+            export(pc, self.test_file_path)
+
+    def test_write_sameFileTwiceOverwrite(self):
+        """ Should not raise an exception """
+        pc = SimpleTestData.get_point_cloud()
+        export(pc, self.test_file_path)
+        export(pc, self.test_file_path, overwrite=True)
+        self.assertTrue(os.path.isfile(self.test_file_path))
+
+    def test_write_loadTheSameSimpleData(self):
+        """ Writing point cloud data and loading it afterwards should result in the same point cloud data. """
+        pc = SimpleTestData.get_point_cloud()
+        export(pc, self.test_file_path)
+        file = _get_file_from_path(self.test_file_path)
+        _assert_all_attributes_in_file(pc[keys.point], file)
+
+    def test_write_loadTheSameComplexData(self):
+        """ Writing point cloud data and loading it afterwards should result in the same point cloud data. """
+        test_data = ComplexTestData()
+        pc = test_data.get_point_cloud()
+        export(pc, self.test_file_path)
+        file = _get_file_from_path(self.test_file_path)
+        _assert_all_attributes_in_file(pc[keys.point], file)
+
+    def test_write_loadRealData(self):
+        """ Writing point cloud data and loading it afterwards should result in the same point cloud data. """
+        pc = load(os.path.join(self._test_data_source, 'AHN3.las'))
+        export(pc, self.test_file_path)
+        file = _get_file_from_path(self.test_file_path)
+        _assert_all_attributes_in_file(pc[keys.point], file)
+
+    def test_write_processedRealData(self):
+        """ Writing point cloud data and loading it afterwards should result in the same point cloud data. """
+        pc = load(os.path.join(self._test_data_source, 'AHN3.las'))
+        x = pc[keys.point]['x']['data']
+        pc[keys.point]['test_feature'] = {'data': np.zeros_like(x), 'type': 'float64'}
+        export(pc, self.test_file_path)
+        file = _get_file_from_path(self.test_file_path)
+        _assert_all_attributes_in_file(pc[keys.point], file)
+
+    def setUp(self):
+        os.mkdir(self._test_dir)
+
+    def tearDown(self):
+        shutil.rmtree(self._test_dir)
+
+
+def _assert_all_attributes_in_file(attributes, file):
+    for name, attribute in attributes.items():
+        data = attribute['data']
+        dtype = np.dtype(attribute['type'])
+        assert hasattr(file, name)
+        file_data = getattr(file, name)
+        assert dtype.name == file_data.dtype.name
+        assert data.size == file_data.size
+        np.testing.assert_allclose(data, file_data, err_msg="{} differ".format(name))
+
+
+def _get_file_from_path(path):
+    return laspy.file.File(path)
