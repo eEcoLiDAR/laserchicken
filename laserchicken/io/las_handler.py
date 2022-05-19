792f76d661ef00d36af4a6146ffd51e6ce5c41e8
Francesco Nattino <f.nattino@esciencecenter.nl>
2022-05-19 22:50:47 +0200

    pylas -> laspy for LAS/LAZ files

--- laserchicken/io/las_handler.py
+++ laserchicken/io/las_handler.py
@@ -1,9 +1,9 @@
 """ IO Handler for LAS (and compressed LAZ) file format """
-import pylas
+import laspy
 
 from laserchicken import keys
 from laserchicken.io.base_io_handler import IOHandler
-from laserchicken.io.utils import convert_to_short_type, select_valid_attributes
+from laserchicken.io.utils import select_valid_attributes
 
 
 DEFAULT_LAS_ATTRIBUTES = {
@@ -26,31 +26,35 @@ class LASHandler(IOHandler):
         :param attributes: list of attributes to read ('all' for all attributes in file)
         :return: point cloud data structure
         """
-        file = pylas.read(self.path)
+        file = laspy.read(self.path)
+        dtype = file.header.point_format.dtype()
         attributes_available = [el if el not in ['X', 'Y', 'Z'] else el.lower()
-                                for el in file.points.dtype.names]
+                                for el in dtype.fields.keys()]
 
         attributes = select_valid_attributes(attributes_available, attributes)
 
         points = {}
         for name in attributes:
             if hasattr(file, name):
-                data = getattr(file, name)
+                if name in 'xyz':
+                    data = getattr(file, name).scaled_array()
+                else:
+                    data = getattr(file, name)
                 points[name] = _get_attribute(data, data.dtype.name)
 
         return {keys.point: points}
 
-    def write(self, point_cloud, attributes='all', file_version='1.2', point_format_id=3):
+    def write(self, point_cloud, attributes='all', file_version='1.2', point_format=3):
         """
         Write point cloud to a LAS(LAZ) file.
 
         :param point_cloud:
         :param attributes: list of attributes to write ('all' for all attributes in point_cloud)
         :param file_version:
-        :param point_format_id:
+        :param point_format:
         :return:
         """
-        file = pylas.create(point_format_id=point_format_id,
+        file = laspy.create(point_format=point_format,
                             file_version=file_version)
 
         points = point_cloud[keys.point]
@@ -58,17 +62,23 @@ class LASHandler(IOHandler):
 
         # NOTE: adding extra dims and assignment should be done in two steps,
         # some fields (e.g. raw_classification) are otherwise overwritten
+        dtype = file.header.point_format.dtype()
         for attribute in attributes:
             data, type = _get_data_and_type(points[attribute])
-            type_short = convert_to_short_type(type)
             if attribute not in 'xyz':
                 # x,y,z are not there but file methods can be used to convert coords to int4
-                if attribute not in file.points.dtype.names:
-                    file.add_extra_dim(name=attribute, type=type_short)
-            file_type_short = convert_to_short_type(getattr(file, attribute).dtype.name)
-            if not file_type_short == type_short:
+                if attribute not in dtype.fields:
+                    param = laspy.ExtraBytesParams(name=attribute, type=type)
+                    file.add_extra_dim(param)
+            file_type = getattr(file, attribute).dtype.name
+            if not file_type == type:
                 raise TypeError('Data type in file does not match the one in point cloud: '
-                                'for {}, {} vs {}'.format(attribute, file_type_short, type_short))
+                                'for {}, {} vs {}'.format(attribute, file_type, type))
+
+        for dim in 'xyz':
+            data, _ = _get_data_and_type(points[dim])
+            setattr(file.header, '{}_offset'.format(dim), data.min())
+            setattr(file.header, '{}_scale'.format(dim), 0.001)
 
         for attribute in attributes:
             data, _ = _get_data_and_type(points[attribute])
@@ -81,7 +91,7 @@ class LASHandler(IOHandler):
             file.write(self.path)
         except ValueError as err:
             raise ValueError('Error in writing LAS file (file_version {}, point_format_id {}). '
-                             'pylas error below:\n{}'.format(file_version, point_format_id, err))
+                             'laspy error below:\n{}'.format(file_version, point_format, err))
 
 
 def _get_attribute(data, data_type):
