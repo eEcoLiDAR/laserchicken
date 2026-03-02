f7ff66cc1764622c8de718a3a84abd4141dbe8b4
Francesco Nattino <f.nattino@esciencecenter.nl>
2026-03-02 13:47:44 +0100

    update package metadata

--- setup.py
+++ setup.py
@@ -1,32 +1,3 @@
-import os
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-
-def read(file_name):
-    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
-
-
-required = read('requirements.txt').splitlines()
-
-setup(
-    name='laserchicken',
-    version='0.7.0',
-    description='Point cloud toolkit',
-    license='Apache 2.0',
-    keywords=['Python', 'Point cloud'],
-    url='https://github.com/eEcoLiDAR/eEcoLiDAR',
-    packages=find_packages(),
-    install_requires=required,
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
-    classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-    ],
-    entry_points={
-        'console_scripts': [
-            'laserchicken = laserchicken.tools.cli:main',
-        ],
-    },
-)
+setup()
