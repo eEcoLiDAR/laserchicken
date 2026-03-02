94fa9f3eb9a2c435bdecd08718a75a8901e9117f
Francesco Nattino <f.nattino@esciencecenter.nl>
2026-03-02 12:22:29 +0100

    fix deprecation of WKTReadingError

--- laserchicken/filter.py
+++ laserchicken/filter.py
@@ -6,7 +6,7 @@ import shapefile
 import shapely
 import sys
 
-from shapely.errors import WKTReadingError
+from shapely.errors import ShapelyError
 from shapely.wkt import loads
 from shapely.geometry import box
 from shapely.vectorized import contains
@@ -115,7 +115,7 @@ def select_polygon(point_cloud, polygon_string, read_from_file=False, return_mas
         polygon = reader(polygon_string)
     else:
         polygon = _load_polygon(polygon_string)
-    
+
     if isinstance(polygon, shapely.geometry.polygon.Polygon):
         points_in = _contains(point_cloud, polygon)
     elif isinstance(polygon,shapely.geometry.multipolygon.MultiPolygon):
@@ -129,8 +129,8 @@ def select_polygon(point_cloud, polygon_string, read_from_file=False, return_mas
         print('{} points found in {} polygons.'.format(len(points_in), len(polygon.geoms)))
     else:
         raise ValueError('It is not a Polygon or Multipolygon.')
-    
-    if return_mask: 
+
+    if return_mask:
         mask = np.zeros(len(point_cloud['vertex']['x']['data']), dtype=bool)
         mask[points_in] = True
         return mask
@@ -177,7 +177,7 @@ def _get_polygon_reader(format):
 def _load_polygon(string):
     try:
         return loads(string)
-    except WKTReadingError:
+    except ShapelyError:
         raise ValueError('Polygon is invalid. --> {}'.format(string))
 
 
